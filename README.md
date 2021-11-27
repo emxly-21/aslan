2f8c3612f63a950c48d8137fd5e0d95a860321ab
Kevin Hsu <kevin.hsukaihao@gmail.com>
2021-11-27 03:44:02 -0500

    Bug fix and add other dataset preprocessing

--- README.md
+++ README.md
@@ -2,8 +2,20 @@
 A convolutional neural network model to recognize the letters of the American Sign Language alphabet
 
 
-## Downloading the dataset
+## Downloading the datasets
 
+### Sign Language MNIST
 - Head over to this [link](https://www.kaggle.com/datamunge/sign-language-mnist/download) with a valid Kaggle account to download the dataset. 
-- Unzip the downloaded `archive.zip`, rename the directory to `data/`, and move it to the root of the project directory
+- Unzip the downloaded `archive.zip`, rename the directory to `data/`, and move it to the project root directory
+
+
+### ASL Finger Spelling Dataset
+- Heads up! The dataset is fairly large (2.25 GB)
+- Run `wget https://www.cvssp.org/FingerSpellingKinect2011/fingerspelling5.tar.bz2` in the project root directory
+- Extract the file by running `tar xvjf fingerspelling5.tar.bz2`
+
+
+## Preprocessing
+- `get_data` can be used to retrieve the Sign Language MNIST dataset
+- `get_data_2` can be used to retrieve the ASL Finger Spelling dataset
 
--- code/preprocess.py
+++ code/preprocess.py
@@ -1,8 +1,10 @@
 
 from csv import reader
 import numpy as np
+from PIL import Image
+import os
 
-
+## Sign Language MNIST
 def get_data(file_path): 
     """
     Returns two numpy arrays, 
@@ -19,18 +21,18 @@ def get_data(file_path):
     Images are 28x28 pixels
     """
 
+    print("reading file_path: ", file_path)
     with open(file_path, 'r') as file:
 
-        csv_data = reader(file)
+        csv_data = list(reader(file))
 
-        num_samples = len(list(csv_data))
+        num_samples = len(csv_data)
         inputsVector = np.empty([num_samples, 784])
         labelsVector = np.empty([num_samples])
         is_header = True
 
         count = 0
         for row in csv_data:
-
             if is_header: 
                 is_header = False
                 continue
@@ -43,10 +45,80 @@ def get_data(file_path):
     return inputsVector, labelsVector
 
 
+def image_to_np_array(file_path): 
+    image = Image.open(file_path).convert('L') # convert to grayscale
+    data = np.asarray(image)
+    return data
+
+
+## ASL Finger Spelling Dataset
+def get_data_2(): 
+    """
+    Returns two numpy arrays, 
+
+    inputs shape:  (num_samples, height, width)
+    labels shape:  (num_samples, height, width)
+
+    Training data (A) contains 12547 samples
+    Training data (A, B) contains 26445 samples
+
+    Example usage: 
+    `inputsVector, labelsVector = get_data()`
+
+    Images have varying widths and heights
+    """
+
+    datasets = ['A/', 'B/']
+    #  datasets = ['A/', 'B/', 'C/', 'D/', 'E/']
+
+    inputsVector = []
+    labelsVector = []
+
+    for letter_index in range(26): 
+        if letter_index in [9, 25]: # ignore j and z
+            continue
+
+        counter = 0
+        letter = chr(letter_index + 97)
+        letter_dir = letter + "/"
+        for a_dataset_dir in datasets: 
+            a_file_path = "../dataset5/" + a_dataset_dir + letter_dir
+            for a_file in os.listdir(a_file_path): 
+                #  image = Image.open(file_path)
+
+                if a_file.startswith("color_"): 
+                    #  print(a_file_path + a_file)
+                    data = image_to_np_array(a_file_path + a_file)
+
+                    inputsVector.append(data)
+                    labelsVector.append(letter_index)
+
+                    #  print(data)
+                    #  print(type(data))
+                    #  print(data.shape)
+                    counter += 1
+        print("letter ", letter, " has ", counter)
+
+    inputsVector = np.array(inputsVector)
+    labelsVector = np.array(labelsVector)
+    #  print(inputsVector)
+    #  print(labelsVector)
+
+    #  print(type(inputsVector))
+    #  print(inputsVector.shape)
+    #  print(type(labelsVector))
+    #  print(labelsVector.shape)
+
+    return inputsVector, labelsVector
+
+
+#  get_data_2()
+
 #  inputsVector, labelsVector = get_data("../data/sign_mnist_train.csv")
 
 #  print(inputsVector)
 #  print(labelsVector)
 #  print("inputs shape: ", inputsVector.shape)
 #  print("labels shape: ", labelsVector.shape)
+
 
