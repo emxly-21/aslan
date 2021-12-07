dbaee4fdd86c7743a7def20376d6f9942bcb3a08
Kevin Hsu <kevin.hsukaihao@gmail.com>
2021-12-07 06:31:38 -0500

    Add j and z to constants to prevent crashes

--- code/constants.py
+++ code/constants.py
@@ -2,9 +2,11 @@
 mnist_dimensions = 28
 sample_dimensions = mnist_dimensions * 10
 
-LETTERS = {1: "a", 2: "b", 3: "c", 4: "d", 5: "e", 6: "f", 7: "g", 8: "h",
+# include j and z to prevent crashes
+LETTERS = { 1: "a", 2: "b", 3: "c", 4: "d", 5: "e", 6: "f", 7: "g", 8: "h",
 9: "i", 11: "k", 12: "l", 13: "m", 14: "n", 15: "o", 16: "p", 17: "q", 18: "r",
-19: "s", 20: "t", 21: "u", 22: "v", 23: "w", 24: "x", 25: "y"}
+19: "s", 20: "t", 21: "u", 22: "v", 23: "w", 24: "x", 25: "y", 
+10: "j", 26: "z" }
 
 
 
