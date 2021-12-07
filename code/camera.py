e517cc8babb005832f3b7267794dd3ba3e979344
Kevin Hsu <kevin.hsukaihao@gmail.com>
2021-12-07 05:16:11 -0500

    Add basic camera code and gitignore

---
+++ code/camera.py
@@ -0,0 +1,36 @@
+
+import numpy as np
+import cv2 as cv
+import time
+
+cap = cv.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+print("cap opened")
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    # Our operations on the frame come here
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    # run gray through the recognizer
+
+    font = cv.FONT_HERSHEY_SIMPLEX
+    cv.putText(frame, 'OpenCV', (10,500), font, 4,(255,255,255),2,cv.LINE_AA)
+
+
+    # Display the resulting frame
+    cv.imshow('frame', frame)
+    if cv.waitKey(1) == ord('q'):
+        break
+    time.sleep(5)
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()
+
