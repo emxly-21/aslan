19f8b0e7df4ba541c33bae9811a97d056112c741
Kevin Hsu <kevin.hsukaihao@gmail.com>
2021-12-07 06:29:49 -0500

    Clean up camera code

--- code/camera.py
+++ code/camera.py
@@ -2,28 +2,10 @@
 import tensorflow as tf
 import numpy as np
 import cv2 as cv
+from constants import *
 
-aslan_model = tf.keras.models.load_model('../model/')
-mnist_dimensions = 28
-sample_dimensions = mnist_dimensions * 10
 
-LETTERS = {1: "a", 2: "b", 3: "c", 4: "d", 5: "e", 6: "f", 7: "g", 8: "h",
-9: "i", 11: "k", 12: "l", 13: "m", 14: "n", 15: "o", 16: "p", 17: "q", 18: "r",
-19: "s", 20: "t", 21: "u", 22: "v", 23: "w", 24: "x", 25: "y"}
-
-cap = cv.VideoCapture(0)
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
-
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-
+def handle_frame(frame): 
     frame = cv.flip(frame, 1)
 
     # get the center 28 by 28 pixels
@@ -37,7 +19,7 @@ while True:
     cropped_frame = frame[frame_x_start:frame_x_end, frame_y_start:frame_y_end, :]
     gray = cv.cvtColor(cropped_frame, cv.COLOR_BGR2GRAY)
     gray = cv.resize(gray, (mnist_dimensions, mnist_dimensions))
-    gray = np.reshape(gray, (1,mnist_dimensions * mnist_dimensions))
+    gray = np.reshape(gray, (1, mnist_dimensions * mnist_dimensions))
 
     # run gray through the recognizer
     logits = aslan_model.predict(gray)
@@ -54,10 +36,29 @@ while True:
 
     # Display the resulting frame
     cv.imshow('frame', frame)
-    if cv.waitKey(200) == ord('q'):
-        break
 
-# When everything done, release the capture
-cap.release()
-cv.destroyAllWindows()
+
+if __name__ == '__main__':
+    aslan_model = tf.keras.models.load_model('../model/')
+
+    cap = cv.VideoCapture(0)
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+
+    while True:
+        ret, frame = cap.read()
+
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+
+        handle_frame(frame)
+
+        if cv.waitKey(200) == ord('q'):
+            break
+
+    # When everything done, release the capture
+    cap.release()
+    cv.destroyAllWindows()
 
