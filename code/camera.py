b902863fb96ead40c7b86031468404f6f768337a
Kevin Hsu <kevin.hsukaihao@gmail.com>
2021-12-07 06:03:21 -0500

    Basic camera

--- code/camera.py
+++ code/camera.py
@@ -1,13 +1,13 @@
 
+import tensorflow as tf
 import numpy as np
 import cv2 as cv
-import time
+aslan_model = tf.keras.models.load_model('../model/')
 
 cap = cv.VideoCapture(0)
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
-print("cap opened")
 
 while True:
     # Capture frame-by-frame
@@ -17,19 +17,26 @@ while True:
         print("Can't receive frame (stream end?). Exiting ...")
         break
     # Our operations on the frame come here
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    print(frame.shape)
+    cropped_frame = frame[0:28, 0:28, :]
+    gray = cv.cvtColor(cropped_frame, cv.COLOR_BGR2GRAY)
+    gray = np.reshape(gray, (1,784))
 
     # run gray through the recognizer
+    logits = aslan_model.predict(gray)
+    output = tf.argmax(logits, 1).numpy()[0]
+    print("logits.shape: ", logits.shape)
+    print(output)
 
-    font = cv.FONT_HERSHEY_SIMPLEX
-    cv.putText(frame, 'OpenCV', (10,500), font, 4,(255,255,255),2,cv.LINE_AA)
+    cv.putText(frame, str(output), (10,500), cv.FONT_HERSHEY_SIMPLEX, 4, (255,255,255), 2, cv.LINE_AA)
 
 
     # Display the resulting frame
     cv.imshow('frame', frame)
-    if cv.waitKey(1) == ord('q'):
+    if cv.waitKey(1000) == ord('q'):
         break
-    time.sleep(5)
+
 # When everything done, release the capture
 cap.release()
 cv.destroyAllWindows()
