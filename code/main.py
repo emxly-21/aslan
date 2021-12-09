70bdef26ecb8110ea9d690c1d2caeaedb1ba413f
GarethMansfield <gareth_mansfield@brown.edu>
2021-12-08 22:52:16 -0500

    About to merge

--- code/main.py
+++ code/main.py
@@ -5,7 +5,7 @@ import numpy as np
 import random
 import math
 from model import Model
-from preprocess import get_data, get_data_2
+from preprocess import get_data, get_data_2, get_data_3
 
 def train(model, train_inputs, train_labels):
     indices = tf.random.shuffle(np.arange(len(train_inputs)))
@@ -16,14 +16,11 @@ def train(model, train_inputs, train_labels):
     losses = []
     accuracies = []
     while batch < len(shuffled_inputs):
-        print('1')
         flipped = shuffled_inputs[batch:batch + model.batch_size]
         one_hot_labels = tf.one_hot(tf.cast(shuffled_labels[batch:batch + model.batch_size], tf.uint8), 26, axis=1)
-        print('2')
         with tf.GradientTape() as tape:
             predictions = model.call(flipped)
             loss = model.loss(predictions, one_hot_labels)
-        print('3')
         gradients = tape.gradient(loss, model.trainable_variables)
         model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
         accuracy += model.accuracy(predictions, one_hot_labels)
--- code/preprocess.py
+++ code/preprocess.py
@@ -113,6 +113,67 @@ def get_data_2():
     return inputsVector, labelsVector
 
 
+## ASL Finger Spelling Dataset
+def get_data_3(): 
+    """
+    Returns two numpy arrays, 
+
+    inputs shape:  (num_samples, height, width)
+    labels shape:  (num_samples, height, width)
+
+    Training data (A) contains 12547 samples
+    Training data (A, B) contains 26445 samples
+
+    Example usage: 
+    `inputsVector, labelsVector = get_data()`
+
+    Images have varying widths and heights
+    """
+
+    #  datasets = ['A/', 'B/']
+    datasets = ['A/']#, 'B/', 'C/', 'D/', 'E/']
+
+    inputsVector = []
+    labelsVector = []
+
+    for letter_index in range(3): 
+        if letter_index in [9, 25]: # ignore j and z
+            continue
+
+        counter = 0
+        letter = chr(letter_index + 97)
+        letter_dir = letter + "/"
+        for a_dataset_dir in datasets: 
+            a_file_path = "../data/custom_dataset/" + a_dataset_dir + letter_dir
+            for a_file in os.listdir(a_file_path): 
+                #  image = Image.open(file_path)
+
+                if a_file.startswith("color_"): 
+                    #  print(a_file_path + a_file)
+                    data = image_to_np_array(a_file_path + a_file)
+
+                    inputsVector.append(data)
+                    labelsVector.append(letter_index)
+
+                    #  print(data)
+                    #  print(type(data))
+                    #  print(data.shape)
+                    counter += 1
+        print("letter ", letter, " has ", counter)
+
+    inputsVector = np.stack(inputsVector, axis=0).astype(np.float32) / 255
+    labelsVector = np.stack(labelsVector, axis=0)
+    #  print(inputsVector)
+    #  print(labelsVector)
+
+    #  print(type(inputsVector))
+    #  print(inputsVector.shape)
+    #  print(type(labelsVector))
+    #  print(labelsVector.shape)
+
+    return inputsVector, labelsVector
+
+
 #  get_data_2()
 
 #  inputsVector, labelsVector = get_data("../data/sign_mnist_train.csv")
